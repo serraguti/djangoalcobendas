8531a3fb18a1ef4faf6b484f07f113866b57d860
serraguti <pacoserranox@gmail.com>
2026-02-18 16:24:54 +0100

    aqaa

--- webproject/aplicacion/urls.py
+++ webproject/aplicacion/urls.py
@@ -5,7 +5,9 @@ from aplicacion import views
 #Creamos las rutas
 #http://127.0.0.1:8000/aplicacion
 #http://127.0.0.1:8000/aplicacion/prueba
+#http://127.0.0.1:8000/aplicacion/peliculas
 urlpatterns = [
      path('', views.index, name="index"),
-     path('prueba/', views.prueba, name="prueba")
+     path('prueba/', views.prueba, name="prueba"),
+     path("peliculas/", views.peliculas, name="peliculas"),
 ]
--- webproject/aplicacion/views.py
+++ webproject/aplicacion/views.py
@@ -8,3 +8,6 @@ def index(request):
 
 def prueba(request):
     return render(request, "paginas/prueba.html")
+
+def peliculas(request):
+    return render(request, "paginas/peliculas.html")
