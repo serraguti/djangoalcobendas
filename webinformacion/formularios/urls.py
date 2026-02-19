7340a8f03e901b393f065e093d373a52549dea36
serraguti <pacoserranox@gmail.com>
2026-02-19 16:43:15 +0100

    earweq

--- webinformacion/formularios/urls.py
+++ webinformacion/formularios/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from formularios import views
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('ejemplo/', views.ejemplo, name="ejemplo")
 ]
