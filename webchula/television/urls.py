bddbae4b42f63a9145a6e5cdc3dab0595f830fdb
serraguti <pacoserranox@gmail.com>
2026-02-25 18:10:03 +0100

    up series personajes

--- webchula/television/urls.py
+++ webchula/television/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from television import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("series/", views.series, name="series"),
+    path("personajes/", views.personajes, name="personajes"),
 ]
--- webchula/television/views.py
+++ webchula/television/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
+from television import models as md
 
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+
+def series(request):
+    service = md.ServiceSeries()
+    listaSeries = service.getSeries()
+    informacion = {
+        "listaseries": listaSeries
+    }
+    return render(request, "series.html", informacion)
+
+def personajes(request):
+    if ('idserie' in request.GET):
+        service = md.ServiceSeries()
+        idserie = int(request.GET["idserie"])
+        personajes = service.getPersonajesSerie(idserie)
+        informacion = {
+            "personajes": personajes
+        }
+        return render(request, "personajes.html", informacion)
+    else: 
+        return render(request, "personajes.html")
